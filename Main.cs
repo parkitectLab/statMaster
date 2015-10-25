9fd174e626addb120ba172017c67f6dd7dc7e802
Martin Kelm <martinkelm@shrt.ws>
2015-10-25 14:03:37 +0100

    initial commit with first functions

---
+++ Main.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace StatMaster
+{
+    class Main : IMod
+    {
+        private GameObject _go;
+
+        public void onEnabled()
+        {
+            _go = new GameObject();
+            _go.AddComponent<StatMasterBehaviour>();
+        }
+
+        public void onDisabled()
+        {
+            UnityEngine.Object.Destroy(_go);
+        }
+
+        public string Name
+        {
+            get { return "Stat Master"; }
+        }
+
+        public string Description
+        {
+            get { return "All you need to get your stats mastered!"; }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Parkitect StatMaster
+
+A Parkitect Game Modification which offers the ability to:
+* record / handle
+* save / load
+* manage / export
+
+... all statistics of your game sessions.
+
+(c) Martin 'yokumo' Kelm 2015
